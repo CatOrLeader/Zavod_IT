833e0ddb4cf83b9bda03901fa89c8373d193228c
Artur <arturmuxutdinov@gmail.com>
2024-03-23 16:45:32 +0300

    impl(tenderpro-service):
    created parsing of the tenders in the request

--- src/main/java/innohackatons/zavod_it/controller/TenderController.java
+++ src/main/java/innohackatons/zavod_it/controller/TenderController.java
@@ -2,15 +2,11 @@ package innohackatons.zavod_it.controller;
 
 import innohackatons.zavod_it.dto.TenderDto;
 import innohackatons.zavod_it.service.TenderService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
