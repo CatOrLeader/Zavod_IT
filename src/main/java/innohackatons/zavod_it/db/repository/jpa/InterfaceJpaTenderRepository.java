7d48028a49b321650d8a83912e5badc888094e4a
Vladi <v.grigorev@innopolis.university>
2024-03-28 23:56:27 +0300

    /test

--- src/main/java/innohackatons/zavod_it/db/repository/jpa/InterfaceJpaTenderRepository.java
+++ src/main/java/innohackatons/zavod_it/db/repository/jpa/InterfaceJpaTenderRepository.java
@@ -4,4 +4,5 @@ import innohackatons.zavod_it.dto.TenderDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InterfaceJpaTenderRepository extends JpaRepository<TenderDto, String> {
+
 }
